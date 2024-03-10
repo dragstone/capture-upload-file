64f7e910b580e0ae697fac030afd5a98ef7e6a77
Asmiriti Kumari <asmiriti.kumari@bt.com>
2024-03-10 15:08:39 +0530

    geading added

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        <h1>Upload files</h1>
         <input
           type="file"
           accept="image/*"
